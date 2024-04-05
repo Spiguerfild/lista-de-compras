4e2ee8cc60f0afdd936a2c6aa6fd5f98e176fcfb
Luis Henrique <luique16@outlook.com>
2024-04-04 21:57:37 -0400

    first commit, realizando estrutura base

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <header className="h-[170px] bg-[url('/bg-top-img.png')]  bg-no-repeat bg-center bg-cover">
+      </header>
+
+      <body>
+
+        <div>
+
+        </div>
+
+      </body>
+
+    </>
+  )
+}
+
+export default App
